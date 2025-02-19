1319da1713765a9e45186880daead6e043c91baf
aixiu <4815563@qq.com>
2025-02-19 20:31:05 +0800

    重新修练

--- 07-请求与响应/apps/app02.py
+++ 07-请求与响应/apps/app02.py
@@ -6,7 +6,9 @@ app02 = APIRouter()
 
 @app02.get("/jobs") # 路径参数
 # @app02.get("/jobs/{kd}") # 路径参数
-async def get_jobs(kd, xl:Union[str, None]=None, gj:Optional[str]=None): 
+# async def get_jobs(kd, xl: Union[str, None]=None, gj: Optional[str]=None): 
+async def get_jobs(kd, xl: str | None=None, gj: Optional[str]=None): 
+    # Optional[str] 相当于 Union[str, None]
     # 有没默认值的参数，必须在参数列表中，有默认
     # 基于kd, xl, gj数据库查询岗位信息
     return {
