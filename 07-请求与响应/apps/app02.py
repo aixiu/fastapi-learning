cc7583da336982f7ad278d6dfcb61b0c8325a3eb
aixiu <4815563@qq.com>
2025-02-24 19:16:10 +0800

    新课程

--- 07-请求与响应/apps/app02.py
+++ 07-请求与响应/apps/app02.py
@@ -3,6 +3,14 @@ from typing import Union, Optional
 
 app02 = APIRouter()
 
+# 视频https://www.bilibili.com/video/BV1Ya4y1D7et?spm_id_from=333.788.player.switch&vd_source=20f7c1f5f32f90ae92d9428e45039d9b&p=15  6分45秒有讲解
+# 路径函数中声明不属于路径参数的其他函数参数时，它们将被自动解释为"查询字符串"参数，就是 url? 之后用&分割的 key-value 键值对。
+
+# @app02.get("/jobs/{kd}") 就是路径参数
+# async def get_jobs(kd, xl: str | None=None, gj: Optional[str]=None):   # xl: str | None=None 是查询字符串参数  kd是路径参数
+
+
+
 
 @app02.get("/jobs") # 路径参数
 # @app02.get("/jobs/{kd}") # 路径参数
